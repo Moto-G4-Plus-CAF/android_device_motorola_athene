25295f3fb9ba72c93c2805d840a4fd379cd0a1f5
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2022-10-13 03:04:39 +0530

    athene: copy prebuilt vndk * enable PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE

--- device.mk
+++ device.mk
@@ -367,6 +367,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
